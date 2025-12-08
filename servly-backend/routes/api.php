26ce0d09b3426a48c1f4aa36ee7223648805a42b
Tarek Ahmed <tarekelfanty@gmail.com>
2025-12-08 16:12:29 +0200

    Delete servly-backend directory

--- servly-backend/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-
-Route::post('/signup', [AuthController::class, 'signup']);
-Route::post('/login', [AuthController::class, 'login']);
